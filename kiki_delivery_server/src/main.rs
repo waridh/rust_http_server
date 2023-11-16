c96b74e46ec776fad88d98cc6d7a40d3ee96601d
waridh <waridh@ualberta.ca>
2023-11-16 15:12:11 -0700

    Made the server response work, and added pages to display

--- kiki_delivery_server/src/main.rs
+++ kiki_delivery_server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs, // File system library. Can load files into memory
     io::{prelude::*, BufReader},    // io::prelude is just the IO heavy modules
     net::{TcpListener, TcpStream},
 };
@@ -27,19 +28,37 @@ fn process_request(mut stream: TcpStream) {
     // We want to buffer the reads from stream to improve performance
     let buf_reader = BufReader::new(&mut stream);
 
-    let http_request: Vec<_> = buf_reader.lines()
-        .map(|ele| ele.unwrap())    // The line is wrapped. Just stops program
-        .take_while(|line| !line.is_empty())    // We can stop iterating
-        .collect();                             // Convert back into vector
+    let http_request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write_all(response.as_bytes()).unwrap(); // Hand-coded http response
+    let (status_line, file_name) = if http_request_line == "GET / HTTP/1.1" {
+        // Loading the page
+        let status_line = "HTTP/1.1 200 OK";
+        ("HTTP/1.1 200 OK", "kiki_landing.html")    // The syntax is clean
+    } else {
+        // Enters this when the request is not the basic one
+        // Implement 404 error when this happens
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
+    stream.write_all(response_generator(status_line, contents).as_bytes())
+        .unwrap();
+}
+
+fn response_generator(status_line: &str, content: String) -> String {
+    let length = content.len();
+    let ret = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+    ret
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
+    // function testing for response generation
     #[test]
-    fn place_holder() {
-        assert!(true);
+    fn test_response_generator() {
+        let expected_output1 = String::from("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n");
+        assert_eq!(response_generator("HTTP/1.1 200 OK", String::from("")), expected_output1);
     }
 }
